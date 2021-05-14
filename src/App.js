af6f2db74be4268ac1bd2a5bef6aca30877e2b2f
Joseph M <jmedina1985@hotmail.com>
2021-05-13 21:44:53 -0700

    Display question

--- src/App.js
+++ src/App.js
@@ -25,7 +25,17 @@ function App() {
         </p>
         <article className="container">
           <h2 dangerouslySetInnerHTML={{ __html: question }} />
-          <div className="btn-container">{correct.map((answer) => {})}</div>
+          <div className="btn-container">
+            {correct.map((answer, index) => {
+              return (
+                <button
+                  key={index}
+                  className="answer-btn"
+                  dangerouslySetInnerHTML={{ __html: answer }}
+                />
+              );
+            })}
+          </div>
         </article>
       </section>
     </main>
