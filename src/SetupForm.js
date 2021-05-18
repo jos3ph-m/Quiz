aeb41d8a1be7ce19fff1ed3e1c6718f6908fbf33
Joseph M <jmedina1985@hotmail.com>
2021-05-17 21:44:43 -0700

    Setup Form structure

--- src/SetupForm.js
+++ src/SetupForm.js
@@ -1,8 +1,9 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from 'react';
+import { useGlobalContext } from './context';
 
 const SetupForm = () => {
-  return <h2>setup form</h2>
-}
+  const { quiz, handleChange, handleSubmit, error } = useGlobalContext();
+  return <h2>setup form</h2>;
+};
 
-export default SetupForm
+export default SetupForm;
