c76b75f20902ed1f2e6fea571f627cc237147957
Joseph M <jmedina1985@hotmail.com>
2021-05-18 22:02:31 -0700

    Setup Form structure

--- src/SetupForm.js
+++ src/SetupForm.js
@@ -10,7 +10,7 @@ const SetupForm = () => {
           {/*amount*/}
           <div className="form-control">
             <label htmlFor="amount">number of questions</label>
-            <input type="text" name="" id="" />
+            <input type="text" name="amount" id="amount" />
           </div>
         </form>
       </section>
