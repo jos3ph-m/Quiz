d85a914117fc33aeb88eaf806183bdfd68e9d85f
Joseph M <jmedina1985@hotmail.com>
2021-05-20 22:02:13 -0700

    handle submit

--- src/context.js
+++ src/context.js
@@ -88,7 +88,7 @@ const AppProvider = ({ children }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     const { amount, category, difficulty } = quiz;
-    const url = `${API_ENDPOINT}amount=${amount}&category=${category}&difficulty=${difficulty}&type=multiple`;
+    const url = `${API_ENDPOINT}amount=${amount}&category=${table[category]}&difficulty=${difficulty}&type=multiple`;
   };
 
   return (
