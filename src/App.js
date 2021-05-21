311a4375b27ec8c0510f8fd9ddd8c3e70583c9b1
Joseph M <jmedina1985@hotmail.com>
2021-05-21 10:22:28 -0700

    Randomize correct answers

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
   }
 
   const { question, incorrect_answers, correct_answer } = questions[index];
-  // const answers = [...incorrect_answers, correct_answer];
+
   let answers = [...incorrect_answers];
   const tempIndex = Math.floor(Math.random() * 4);
   if (tempIndex === 3) {
