b513fc1a3fa9657866ec8d4888dfeaf8bccf71e9
Joseph M <jmedina1985@hotmail.com>
2021-05-20 21:51:40 -0700

    Setup Form structure

--- src/context.js
+++ src/context.js
@@ -82,6 +82,7 @@ const AppProvider = ({ children }) => {
   const handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
+    console.log(name, value);
   };
 
   const handleSubmit = (e) => {
